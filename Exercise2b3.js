cd86f167a53986f70506d1ebecbb570398bf1050
NguyễnHữuTài <50362109+gambo2007@users.noreply.github.com>
2023-11-15 14:40:29 +0700

    Delete Exercise2b3.js

--- Exercise2b3.js
+++
@@ -1,9 +0,0 @@
-function randomArray(arr){
-    if (arr.length === 0){
-        return undefined;
-    }
-    const rndArray = Math.floor(Math.random() * arr.length);
-    return arr[rndArray];
-}
-
-console.log(randomArray([2,3,4,5,6,7]));
