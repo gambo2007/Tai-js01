93b88170a1579dc235156c0e1cb16e12cb52fa65
NguyễnHữuTài <50362109+gambo2007@users.noreply.github.com>
2023-11-15 14:14:54 +0700

    Update Exercise2b2.js

--- Exercise2b2.js
+++ Exercise2b2.js
@@ -1,9 +1,3 @@
-// Trường hop min va max la nguyen duong
-function random(min,max){
-    return Math.floor(Math.random()*(max-min +1)) + min;
-}
-
-// Trường hợp min va max nguyen âm hoac thap phan
 function random2(min,max){
     min = Math.ceil(min);
     max = Math.round(max);
