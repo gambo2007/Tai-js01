b931297c2e164c2d1b05830138cf5735a1727ce0
NguyễnHữuTài <50362109+gambo2007@users.noreply.github.com>
2023-11-15 14:40:19 +0700

    Delete Exercise2b2.js

--- Exercise2b2.js
+++
@@ -1,7 +0,0 @@
-function random2(min,max){
-    min = Math.ceil(min);
-    max = Math.round(max);
-    return Math.floor(Math.random()*(max-min +1)) + min;
-}
-console.log(random(1,25))
-console.log(random2(1,25))
